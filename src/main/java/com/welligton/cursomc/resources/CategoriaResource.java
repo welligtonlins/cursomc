afe6158ec39facf440315861bfb025bdec34aaf7
Welligton Lins <welligtonlinsufac@gmail.com>
2019-12-20 13:25:58 -0300

    Testando classe Categoria

--- src/main/java/com/welligton/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/welligton/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.welligton.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.welligton.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value= "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method= RequestMethod.GET)
-	public String listar() {
-		return "REST funcionou corretamente!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Banho");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+
 	}
 	
 }
